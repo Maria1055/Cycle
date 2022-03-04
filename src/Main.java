9fb67ea0d329fcef2a83ebda919e96617b86f525
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-05 01:46:51 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        for (a = 100; a >= 1;  a--)
+            if (a % 4 == 0) {
+                System.out.println("Числа четные 4: " + a);
+            }
+    }
+}
