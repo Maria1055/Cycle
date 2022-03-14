3eef03c25b2899a5090665f272f36bead97841c9
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-14 23:53:59 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int i =0;
+        int i = 0;
         System.out.println(i % 4 == 0);
-        for (i = 100; i >= 1; i--)
-        {
-        System.out.println("Числа четные 4: " + i);
+        for (i = 100; i >= 1; i--) {
+            if (i % 4 == 0) {
+                System.out.println("Числа четные 4: " + i);
+            }
         }
 
     }
