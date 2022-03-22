ae0121058b5691d564367805de8cb9ef1a02d65b
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-22 20:06:49 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
+    public static int fourLoopNumbers(int i) {
+        return i % 4 == 0;
+    }
     public static void main(String[] args) {
-        int i = 0;
-        System.out.println(i % 4 == 0);
+        int i = fourLoopNumbers();
         for (i = 100; i >= 1; i--) {
-            if (i % 4 == 0) {
-                System.out.println("Числа четные 4: " + i);
-            }
+            System.out.println("Числа кратные 4:" + i);
         }
-
     }
 }
+
